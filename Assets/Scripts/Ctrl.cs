2c5867d905b27126cf90fea103f8c236a97f166d
BeKaLo <910626231@qq.com>
2016-11-23 17:26:31 +0800

    worry

---
+++ Assets/Scripts/Ctrl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ctrl : MonoBehaviour {
+	public static Ctrl Instance = null;
+	// Use this for initialization
+	void Awake(){
+		Instance = this;
+		DontDestroyOnLoad (this);
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Init_Ctrl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using DragonBones;
+
+public class Init_Ctrl : MonoBehaviour {
+	public static Init_Ctrl Instance = null;
+	// Use this for initialization
+	void Awake(){
+		Instance = this;
+	}
+	void Start () {
+		DBinit ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void DBinit(){
+		// Load data.
+		UnityFactory.factory.LoadDragonBonesData("DragonBones/Dragon_ske"); // DragonBones file path (without suffix)
+		UnityFactory.factory.LoadTextureAtlasData("DragonBones/Dragon_tex"); //Texture atlas file path (without suffix) 
+		// Create armature.
+		var armatureComponent =UnityFactory.factory.BuildArmatureComponent("Dragon"); // Input armature name
+		// Play animation.
+		armatureComponent.animation.Play("walk");
+		//UnityFactory.factory.ParseDragonBonesData("DragonBones/Dragon_ske");
+		//UnityFactory.factory.ParseTextureAtlasData("DragonBones/Dragon_tex");
+		UnityFactory.factory.ReplaceSlotDisplay(null,"Dragon", "legL","parts/legL", armatureComponent.armature.GetSlot("handL"));
+
+		// Change armatureposition.
+		armatureComponent.transform.localPosition = new Vector3(0.0f, 0.0f,0.0f);
+
+	}
+}
